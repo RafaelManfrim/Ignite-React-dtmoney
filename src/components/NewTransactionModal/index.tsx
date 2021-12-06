c0f5a94b177fe15e5ea416d6e07a8ab15f2f5a39
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2021-12-06 13:37:42 -0300

    Estilizando Modal de forma global

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 Modal.setAppElement('#root')
 
@@ -9,8 +10,19 @@ type NewTransactionModalProps = {
 
 export const NewTransactionModal = ({ isOpen, onRequestClose }: NewTransactionModalProps) => {
     return (
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-            <h2>Cadastrar transação</h2>
+        <Modal 
+            isOpen={isOpen} 
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <Container>
+                <h2>Cadastrar transação</h2>
+                <input type="text" placeholder="Título"/>
+                <input type="number" placeholder="Valor"/>
+                <input type="text" placeholder="Categoria"/>
+                <button type="submit">Cadastrar</button>
+            </Container>
         </Modal>
     )
 }
