b4cb45497f392dafc6e421f0e92c53bc407614f6
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2021-12-07 22:12:43 -0300

    Configuração nova rota post e db no mirageJs

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de sistema',
+          type: 'deposit',
+          amount: 6000,
+          category: 'Trabalho',
+          createdAt: new Date('2021-11-11 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Compra de monitor',
+          type: 'withdraw',
+          amount: 1500,
+          category: 'Investimento',
+          createdAt: new Date('2021-12-12 11:30:30'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api'
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
     })
   }
 })
